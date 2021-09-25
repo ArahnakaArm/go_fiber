663fda6c3fa1cb13b347b961235b19db053b43e9
Teetawat <teetawatriya@gmail.com>
2021-09-26 01:27:00 +0700

    add many example , add auth , add middleware

--- main.go
+++ main.go
@@ -1,5 +1,319 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"github.com/jmoiron/sqlx"
+	"golang.org/x/crypto/bcrypt"
+
+	jwtware "github.com/gofiber/jwt/v2"
+)
+
+var db *sqlx.DB
+
+const jwtSecret = "teetawat"
+
 func main() {
 
+	var err error
+	db, err = sqlx.Open("mysql", "root:12345678@tcp/go_fiber")
+
+	if err != nil {
+		panic(err)
+	}
+
+	app := fiber.New()
+
+	app.Use("/hello", jwtware.New(jwtware.Config{
+		SigningMethod: "HS256",
+		SigningKey:    []byte(jwtSecret),
+		SuccessHandler: func(c *fiber.Ctx) error {
+			return c.Next()
+		},
+		ErrorHandler: func(c *fiber.Ctx, e error) error {
+			return fiber.ErrUnauthorized
+		},
+	}))
+
+	app.Post("/signup", Signup)
+	app.Post("/login", Login)
+	app.Get("/hello", Hello)
+
+	app.Listen(":8000")
+}
+
+func Signup(c *fiber.Ctx) error {
+	request := SignupRequest{}
+	err := c.BodyParser(&request)
+	if err != nil {
+		return err
+	}
+
+	if request.Username == "" || request.Password == "" {
+		return fiber.ErrUnprocessableEntity
+	}
+
+	password, err := bcrypt.GenerateFromPassword([]byte(request.Password), 10)
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
+	}
+
+	query := "insert into user (username , password) values (? , ?)"
+	result, err := db.Exec(query, request.Username, string(password))
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
+	}
+
+	user := User{
+		Id:       int(id),
+		Username: request.Username,
+		Password: string(password),
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	request := LoginRequest{}
+
+	err := c.BodyParser(&request)
+
+	if err != nil {
+		return err
+	}
+
+	if request.Username == "" || request.Password == "" {
+		return fiber.ErrUnprocessableEntity
+	}
+
+	user := User{}
+
+	query := "select id , username , password from user where username=?"
+	err = db.Get(&user, query, request.Username)
+	if err != nil {
+		return fiber.NewError(fiber.StatusNotFound, "Incorrect username or password")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password))
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusNotFound, "Incorrect username or password")
+	}
+
+	cliams := jwt.StandardClaims{
+		Issuer:    strconv.Itoa(user.Id),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, cliams)
+	token, err := jwtToken.SignedString([]byte(jwtSecret))
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(fiber.Map{
+		"token": token,
+	})
+
+}
+
+func Hello(c *fiber.Ctx) error {
+	return c.JSON(fmt.Sprintf("Hello"))
+}
+
+type User struct {
+	Id       int    `db:"id" json:"id"`
+	Username string `db:"username" json:"username"`
+	Password string `db:"password" json:"password"`
+}
+
+type SignupRequest struct {
+	Username string `db:"username"`
+	Password string `db:"password"`
+}
+
+type LoginRequest struct {
+	Username string `db:"username"`
+	Password string `db:"password"`
+}
+
+func Fiber() {
+	app := fiber.New(fiber.Config{
+		Prefork: true,
+	})
+
+	//MIDDLEWARE
+	app.Use(func(c *fiber.Ctx) error {
+		c.Locals("name", "arm")
+		fmt.Println("BEFORE")
+		err := c.Next()
+		fmt.Println("AFTER")
+		return err
+	})
+
+	app.Use(requestid.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "*",
+		AllowHeaders: "*",
+	}))
+
+	app.Use(logger.New(logger.Config{
+		TimeZone: "asia/Bankok",
+	}))
+
+	//GET
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		name := c.Locals("name")
+		fmt.Println("Get Hello World")
+		return c.SendString(fmt.Sprintf("GET : Hello %v", name))
+	})
+
+	//POST
+	app.Post("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Post Hello world")
+	})
+
+	//PARAMETER OPTIONAL?
+	app.Get("/hello/:name/:surname", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		surname := c.Params("surname")
+		return c.SendString("name : " + name + " ,surname : " + surname)
+	})
+
+	//PARAM INT
+	app.Get("helloint/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+		return c.SendString(fmt.Sprintf("ID = %v", id))
+
+	})
+
+	//QUERY
+	app.Get("/query", func(c *fiber.Ctx) error {
+		name := c.Query("name")
+		surname := c.Query("surname")
+		return c.SendString("name = " + name + " , surname = " + surname)
+	})
+
+	//QUERY 2
+	app.Get("/query2", func(c *fiber.Ctx) error {
+		person := Person{}
+		c.QueryParser(&person)
+		return c.JSON(person)
+	})
+
+	//WILDCARD
+	app.Get("wildcards/*", func(c *fiber.Ctx) error {
+		wildcard := c.Params("*")
+		return c.SendString(wildcard)
+
+	})
+
+	//STATIC FILE
+	app.Static("/", "./wwwroot", fiber.Static{
+		Index:         "idex.html",
+		CacheDuration: time.Second * 10,
+	})
+
+	//NEW ERROR
+	app.Get("/error", func(c *fiber.Ctx) error {
+		return fiber.NewError(fiber.StatusNotFound, "content not found")
+	})
+
+	//GROUP
+	v1 := app.Group("/v1", func(c *fiber.Ctx) error {
+		c.Set("Version", "v1")
+		return c.Next()
+	})
+	v1.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello v1")
+	})
+
+	v2 := app.Group("/v2", func(c *fiber.Ctx) error {
+		c.Set("Version", "v2")
+		return c.Next()
+	})
+	v2.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello v2")
+	})
+
+	//MOUNT
+	userApp := fiber.New()
+	userApp.Get("/login", func(c *fiber.Ctx) error {
+		return c.SendString("login")
+	})
+
+	//SERVER
+	app.Server().MaxConnsPerIP = 1
+	app.Get("/server", func(c *fiber.Ctx) error {
+		time.Sleep(time.Second * 30)
+		return c.SendString("server")
+	})
+
+	//ENV
+	app.Get("/env", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"BaseURL":     c.BaseURL(),
+			"Hostname":    c.Hostname(),
+			"IP":          c.IP(),
+			"IPs":         c.IPs(),
+			"OriginalURL": c.OriginalURL(),
+			"Path":        c.Path(),
+			"Protocol":    c.Protocol(),
+			"Subdomains":  c.Subdomains(),
+		})
+	})
+
+	//BODY
+	app.Post("/body", func(c *fiber.Ctx) error {
+		fmt.Println(string(c.Body()))
+		person := Person{}
+		err := c.BodyParser(&person)
+		if err != nil {
+			return err
+		}
+		fmt.Println(person)
+		return nil
+	})
+
+	app.Post("/body2", func(c *fiber.Ctx) error {
+		/* 	fmt.Println(string(c.Body())) */
+		data := map[string]interface{}{}
+		err := c.BodyParser(&data)
+		if err != nil {
+			return err
+		}
+		fmt.Println(data)
+		return nil
+	})
+
+	app.Mount("/user", userApp)
+
+	app.Listen(":8000")
+
+}
+
+type Person struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
